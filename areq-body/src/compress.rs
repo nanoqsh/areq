108f0c1f70592d5da0cf6ec0f1935a92eaeac07d
nanoqsh <nanoqsh@gmail.com>
2025-02-19 02:30:57 +0500

    feat: add body compression encoding

--- areq-body/src/compress.rs
+++ areq-body/src/compress.rs
@@ -1,38 +1,121 @@
 use {
-    crate::body::{Body, BodyExt, Hint},
-    async_compression::futures::bufread,
-    bytes::{Bytes, BytesMut},
-    futures_lite::{io::BufReader, prelude::*},
-    std::{io::Error, pin::Pin},
+    crate::body::{Body, Hint},
+    async_compression::futures::{bufread, write},
+    bytes::{Buf, Bytes, BytesMut},
+    futures_lite::{io::BufReader, prelude::*, AsyncWriteExt},
+    std::{
+        io::Error,
+        pin::Pin,
+        task::{Context, Poll},
+    },
 };
 
-type BoxedBufRead<'body> = Pin<Box<dyn AsyncBufRead + 'body>>;
+pub struct GzipEncode<B> {
+    end: bool,
+    enc: write::GzipEncoder<Writer>,
+    body: B,
+}
 
-pub struct GzipDecode<'body> {
-    dec: bufread::GzipDecoder<BoxedBufRead<'body>>,
+impl<B> GzipEncode<B> {
+    pub fn new(body: B) -> Self {
+        let write = Writer(BytesMut::new());
+        let enc = write::GzipEncoder::new(write);
+        Self {
+            end: false,
+            enc,
+            body,
+        }
+    }
+}
+
+impl<B> Body for GzipEncode<B>
+where
+    B: Body,
+{
+    type Chunk = Bytes;
+
+    async fn chunk(&mut self) -> Option<Result<Self::Chunk, Error>> {
+        if self.end {
+            return None;
+        }
+
+        let chunk = match self.body.chunk().await {
+            Some(Ok(chunk)) => chunk,
+            Some(Err(e)) => return Some(Err(e)),
+            None => {
+                self.end = true;
+
+                self.enc.flush().await.expect("infallible flush");
+                let bytes = self.enc.get_mut().0.split().freeze();
+                return Some(Ok(bytes));
+            }
+        };
+
+        let buf = chunk.chunk();
+        if buf.is_empty() {
+            return Some(Ok(Bytes::new()));
+        }
+
+        self.enc.write_all(buf).await.expect("infallible write");
+        let bytes = self.enc.get_mut().0.split().freeze();
+        Some(Ok(bytes))
+    }
+
+    fn size_hint(&self) -> Hint {
+        Hint::Chunked { end: false }
+    }
+}
+
+struct Writer(BytesMut);
+
+impl AsyncWrite for Writer {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        _: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Error>> {
+        self.0.extend_from_slice(buf);
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+}
+
+pub struct GzipDecode<R> {
+    dec: bufread::GzipDecoder<R>,
     buf: BytesMut,
 }
 
-impl<'body> GzipDecode<'body> {
-    pub fn new<B>(body: B) -> Self
+impl<R> GzipDecode<R> {
+    pub fn from_buf_read(read: R) -> Self
     where
-        B: Body + 'body,
+        R: AsyncBufRead + Unpin,
     {
-        Self::from_buf_read(BufReader::new(body.read()))
-    }
-
-    pub fn from_buf_read<R>(read: R) -> Self
-    where
-        R: AsyncBufRead + 'body,
-    {
-        let read: BoxedBufRead<'body> = Box::pin(read);
         let dec = bufread::GzipDecoder::new(read);
         let buf = BytesMut::new();
         Self { dec, buf }
     }
 }
 
-impl Body for GzipDecode<'_> {
+impl<R> GzipDecode<BufReader<R>> {
+    pub fn from_read(read: R) -> Self
+    where
+        R: AsyncRead + Unpin,
+    {
+        Self::from_buf_read(BufReader::new(read))
+    }
+}
+
+impl<R> Body for GzipDecode<R>
+where
+    R: AsyncBufRead + Unpin,
+{
     type Chunk = Bytes;
 
     async fn chunk(&mut self) -> Option<Result<Self::Chunk, Error>> {
@@ -56,12 +139,28 @@ impl Body for GzipDecode<'_> {
 
 #[cfg(test)]
 mod tests {
-    use {super::*, futures_lite::future};
+    use {super::*, crate::body::BodyExt, futures_lite::future, std::pin};
+
+    #[test]
+    fn gzip_encode() {
+        let s = "hello world!";
+        let enc = GzipEncode::new(s);
+        let out = future::block_on(enc.bytes()).expect("encode body");
+
+        assert_eq!(
+            &*out,
+            [
+                31, 139, 8, 0, 0, 0, 0, 0, 0, 255, 202, 72, 205, 201, 201, 87, 40, 207, 47, 202,
+                73, 81, 4, 0, 0, 0, 255, 255,
+            ]
+        );
+    }
 
     #[test]
     fn gzip_decode() {
         let s = include_bytes!("../test/hello.gzip");
-        let dec = GzipDecode::new(s.as_slice());
+        let body = pin::pin!(s.as_slice().read());
+        let dec = GzipDecode::from_read(body);
         let out = future::block_on(dec.text()).expect("decode body");
         assert_eq!(out, "hello world!");
     }
